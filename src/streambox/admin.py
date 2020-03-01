2e9c6be6bad76d77bd457897747e3d5b4b433137
ka1ryu <ellegarden356@gmail.com>
2020-03-01 16:35:45 +0900

    db migrate

---
+++ src/streambox/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from streambox.models import Channel
+
+# Register your models here.
+admin.site.register(Channel) 
--- src/sugoi_site/settings.py
+++ src/sugoi_site/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from .settings_secret import *
+import pymysql
+from .secret_settings import *
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'streambox'
 ]
 
 MIDDLEWARE = [
