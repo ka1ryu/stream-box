b93ad492ff2144e24b7a76830d0ecf64d623d3b8
ka1ryu <ellegarden356@gmail.com>
2020-04-12 19:15:54 +0900

    add ignorefile nginx_conf2

--- sql/mysql_init/1_create.sql
+++
@@ -1,7 +0,0 @@
-create database sample;
-use sample;
-
-CREATE TABLE users (
-   id int,
-   name varchar(10)
-);
--- src/streambox/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from streambox.models import Channel
-
-# Register your models here.
-admin.site.register(Channel) 
