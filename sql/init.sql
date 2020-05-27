3a2481468f168f901ce4b85de5e8b4cb49f04f5b
ka1ryu <ellegarden356@gmail.com>
2020-05-28 00:14:17 +0900

    add ignore sqlfolda

--- sql/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS sugoi_site CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-CREATE USER IF NOT EXISTS 'sugoi_site'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON sugoi_site.* TO 'sugoi_site'@'%';
-
-FLUSH PRIVILEGES;
